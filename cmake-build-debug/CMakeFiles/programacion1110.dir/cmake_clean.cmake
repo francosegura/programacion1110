13fd468aa7dcd4f794607424d5d8e8c614143aa4
Franco Segura <franco.ssegura@hotmail.com>
2020-04-14 21:46:33 -0300

    Primer commit

---
+++ cmake-build-debug/CMakeFiles/programacion1110.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programacion1110.dir/main.c.o"
+  "programacion1110"
+  "programacion1110.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/programacion1110.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
